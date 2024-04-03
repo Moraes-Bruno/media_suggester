e68cb4b131c3562587a547d528509985541fa13a
Moraes-Bruno <moraesbruno888@gmail.com>
2024-04-03 11:30:39 -0300

    finalizado front end de detalhes.dart

--- media_suggester/lib/pages/Detalhes.dart
+++ media_suggester/lib/pages/Detalhes.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -98,29 +99,76 @@ class _DetalhesState extends State<Detalhes> {
               const SizedBox(
                 height: 10,
               ),
-              const Padding(
+               Padding(
                 padding: EdgeInsets.all(10),
                 child: Column(
                   children: [
-                    Text(
+                    const Text(
                       "Titulo",
                       style:
                           TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                     ),
-                    Text(
+                    const Text(
                       "Duração|Lançamento|ESRB",
                       style:
                           TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
                     ),
-                    Text(
+                    const Text(
                       "gen1|gen2|gen3|gen4",
                       style:
                           TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
                     ),
-                    SizedBox(
+                    Container(
+                      margin: const EdgeInsets.only(top: 15),
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Criticos',
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(width: 16),
+                              Text(
+                                '|',
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(width: 16),
+                              Text(
+                                'Usuarios',
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 8),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                '*****',
+                                style: TextStyle(fontSize: 25),
+                              ),
+                              SizedBox(width: 50),
+                              Text(
+                                '*****',
+                                style: TextStyle(fontSize: 25),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
                       height: 15,
                     ),
-                    Text(
+                    const Text(
                       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut ",
                       style: TextStyle(fontSize: 20),
                     )
@@ -194,6 +242,27 @@ class _DetalhesState extends State<Detalhes> {
                     ),
                   ],
                 ),
+              ),
+              const SizedBox(height: 15,),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text("Ver Mais",style: TextStyle(fontSize: 20),),
+                  ),
+                ],
+              ),
+              CarouselSlider(
+                items: _obterComentarios(context),
+                options: CarouselOptions(
+                  aspectRatio: 2.0,
+                  enlargeCenterPage: true,
+                  pageViewKey: const PageStorageKey<String>('carousel_slider'),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
               )
             ],
           ),
@@ -204,5 +273,80 @@ class _DetalhesState extends State<Detalhes> {
     if (!await launchUrl(_url)) {
       throw Exception('Could not launch $_url');
     }
+  }
+
+  _obterComentarios(BuildContext context) {
+    final List<String> comments = [
+      "Um filme emocionante que prende sua atenção do início ao fim...",
+      "Uma obra-prima cinematográfica que cativa com sua narrativa envolvente...",
+      "Um filme repleto de ação que entrega adrenalina do começo ao fim...",
+      "Uma comédia hilária que garante gargalhadas do público, com piadas...",
+      "Um drama emocionante que explora temas profundos e complexos..."
+    ];
+
+    final List<Widget> commentSlider = comments.map((comment) {
+      return Builder(
+        builder: (BuildContext context) {
+          return Container(
+            width: MediaQuery.of(context).size.width,
+            margin: const EdgeInsets.only(top: 30),
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 42, 42, 42),
+              borderRadius: BorderRadius.circular(4.0),
+            ),
+            child: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    children: [
+                      Image(
+                        image:
+                            AssetImage("assets/images/profile_placeholder.png"),
+                        height: 45,
+                        width: 45,
+                      ),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Jane Doe",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          Text(
+                            "★★★★☆",
+                            style: TextStyle(fontSize: 20),
+                          )
+                        ],
+                      ),
+                      Spacer(), // Adiciona um espaçamento flexível
+                      Text(
+                        "01/04/2024", // Sua data aqui
+                        style: TextStyle(fontSize: 16),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8, right: 8),
+                  child: Text(
+                    comment,
+                    style: const TextStyle(
+                      fontSize: 20.0,
+                    ),
+                    maxLines: 2,
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      );
+    }).toList();
+
+    return commentSlider;
   }
 }
