e6ffb35e8d72c271a4e8865c8874a2cbc161856b
AnaFMel <anaclara.freire.melo@gmail.com>
2024-04-23 12:48:06 -0300

    Iniciando integração com API do TMDB

--- media_suggester/lib/pages/pesquisa.dart
+++ media_suggester/lib/pages/pesquisa.dart
@@ -1,8 +1,58 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:media_suggester/pages/review_unica.dart';
 
-class Pesquisa extends StatelessWidget {
+class Pesquisa extends StatefulWidget {
   const Pesquisa({super.key});
+
+  @override
+  State<Pesquisa> createState() => _PesquisaState();
+}
+
+class _PesquisaState extends State<Pesquisa> {
+  List<dynamic> movies = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchMovies();
+  }
+
+  Future<void> fetchMovies() async {
+    TextEditingController _searchController = TextEditingController();
+
+    final response = await http.get(
+      Uri.parse(
+          'https://api.themoviedb.org/3/movie/popular?api_key=MY_API_KEY&language=pt-BR'),
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+      setState(() {
+        movies = data['results'];
+      });
+    } else {
+      throw Exception('Nenhum filme encontrado');
+    }
+  }
+
+  Future<void> searchMovies(String query) async {
+    final response = await http.get(
+      Uri.parse(
+          'https://api.themoviedb.org/3/search/movie?query=' + query + '&api_key=MY_API_KEY&language=pt-BR'),
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+      setState(() {
+        movies = data['results'];
+      });
+    } else {
+      throw Exception('Nenhum filme encontrado');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,9 +61,12 @@ class Pesquisa extends StatelessWidget {
         iconTheme: const IconThemeData(color: Colors.white, size: 30),
         backgroundColor: Theme.of(context).colorScheme.primary,
         centerTitle: true,
-        title: const Text(
-          "PESQUISAR",
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        title:  TextField(
+          decoration: InputDecoration(
+            hintText: 'PESQUISAR...',
+            border: InputBorder.none,
+          ),
+          onChanged: searchMovies(),
         ),
         actions: [
           Padding(
@@ -36,28 +89,46 @@ class Pesquisa extends StatelessWidget {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: TextField(
-                style: const TextStyle(fontSize: 20),
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+      body: Column(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: TextField(
+                    style: const TextStyle(fontSize: 20),
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        filled: true,
+                        fillColor: Theme.of(context).colorScheme.inversePrimary,
+                        hintText: "Pesquisar...",
+                        hintStyle: TextStyle(
+                            color: Theme.of(context).colorScheme.tertiary)),
                   ),
-                  filled: true,
-                  fillColor: Theme.of(context).colorScheme.inversePrimary,
-                  hintText: "Pesquisar...",
-                  hintStyle: TextStyle(color: Theme.of(context).colorScheme.tertiary)
-              
                 ),
-                
-              ),
+              ],
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: movies.length,
+              itemBuilder: (context, index) {
+                final movie = movies[index];
+                return ListTile(
+                  title: Text(movie['title']),
+                  subtitle: Text(movie['overview']),
+                  leading: Image.network(
+                    'https://image.tmdb.org/t/p/w200/${movie['poster_path']}',
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
